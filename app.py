975fc5ebb03845094c2fef3b117378e61f98983a
Egor Ivanov <egor.iwanoow@gmail.com>
2020-03-04 00:26:42 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import getWeather
+
+#creating flask web app class
+app = Flask(__name__)
+
+
+#listen for routes
+#listen for index route
+@app.route('/')
+def index_page():
+        return render_template('index.html')
+
+#listen for forecast route
+@app.route('/forecast', methods=['GET'])
+def forecast_page():
+
+
+    #get city name from html form request
+    city = str(request.args['city_input'])
+    temperature = getWeather.getWeather(city)
+
+    #if city is real
+    if temperature != False:
+        return render_template('forecast.html', temperature=temperature)
+
+
+    #if city is not real
+    return render_template('index.html')
---
+++ getWeather.py
@@ -0,0 +1,38 @@
+import requests
+import os
+from geopy.geocoders import Nominatim
+
+def getWeather(city):
+
+    #get city latitude and longitude or None object if city is not real
+    geolocator = Nominatim(user_agent="myWeatherApp")
+    location = geolocator.geocode(city)
+
+    #if city not real quit function
+    if location == None:
+        return False
+
+
+    #dark sky api
+    key = os.environ.get('API_KEY')
+
+    link = 'https://api.darksky.net/forecast/'
+
+    #get data from api
+    url = link + key + '/' + str(location.latitude) + ',' + str(location.longitude) + '?exclude=[minutely, hourly, daily, alerts, flags]'
+
+    #try to make request or quit the function
+    try:
+        #send request
+        r = requests.get(url)
+        data = r.json()
+
+
+        #get temperature, icon from data
+        temperature = data['currently']['temperature']
+        icon = data['currently']['icon']
+
+
+        return temperature
+    except:
+        return False
